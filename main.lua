7446fead1ddfdff74dab76b883bec66a5a135831
arthsmiller <88456630+arthsmiller@users.noreply.github.com>
2022-05-29 18:37:07 +0200

    style

--- main.lua
+++ main.lua
@@ -1,11 +1,9 @@
 function truckSimChatMessageHandler(sender_id, sender_name, message)
-    -- censoring only the exact message 'darn'
-    if message == "darn" then
-        -- cancel the event by returning 1
-        return 1
-    else
-        return 0
-    end
+  if message == "darn" then
+    return 1
+  else
+    return 0
+  end
 end
 
 MP.RegisterEvent("onChatMessage", "truckSimChatMessageHandler")

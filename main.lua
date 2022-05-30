a980ac34018338ecfb0eb8288215ceaa4b7bc6e4
arthsmiller <88456630+arthsmiller@users.noreply.github.com>
2022-05-30 20:01:28 +0200

    chatMSG fix

--- main.lua
+++ main.lua
@@ -1,10 +1,8 @@
 function truckSimCheckSpawnedVehicle(PlayerID, vehicleID, data)
-  print(vehicleID)
-  SendChatMessage(-1, "Spawned Vehicle ID: " + vehicleID)
+  print(vehicleID) --25952
+  SendChatMessage(-1, vehicleID)
 end
 money = 10000;
-
-
 
 MP.RegisterEvent("onVehicleSpawn", "truckSimCheckSpawnedVehicle")
 print(money)
